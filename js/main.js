9cf5baf76568083d4927fe69ba17e4dcdaceef88
Rafael Henrique <fel.rique71@gmail.com>
2021-01-25 19:49:06 -0300

    End

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 
-function soma(n1, n2) {
+function botao() {
+    document.getElementById("thanks").innerHTML = "<b>Obrigado por clicar<b>";
+    //console.log(document.getElementById("thanks"));
+}
+function red() {
+    //window.open("https://github.com/rafuprog/aula-javascript");
+    window.location.href = "https://github.com/rafuprog/aula-javascript";
+}
+function trocar(elemento){
+    //document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+function voltar(elemento){
+    //document.getElementById("mouse").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+function load(){
+    alert("Pagina carregada");
+}
+function change(elemento){
+    console.log(elemento.value);
+}
+/*function soma(n1, n2) {
     return n1 + n2;
 }
 
@@ -16,17 +38,17 @@ var idade = prompt("Qual sua idade?");
 console.log(validaIdade(idade));
 
 
-/*var nome = "rafu";
+var nome = "rafu";
 var n1 = 5;
 var n2 = 3;
 var frase = "Messi é o melhor do mundo"
-//alert(nome  + " tem "+idade+" anos");
-//alert(idade+idade2);
+alert(nome  + " tem "+idade+" anos");
+alert(idade+idade2);
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.replace("Messi", 'Neymar'));
-alert(frase.replace("Messi", 'Neymar'));*/
-/*
+alert(frase.replace("Messi", 'Neymar'));
+
 var list = ["maçã", "pera", "laranja"];
 list.push("uva");
 list.pop("pera");
@@ -34,26 +56,26 @@ console.log(list);
 console.log(list.length);
 console.log(list.toString());
 console.log(list.join("  "));
-*/
-/*var fruta = {nome: "Maça", cor: "Vermelha"};
+
+var fruta = {nome: "Maça", cor: "Vermelha"};
 console.log(fruta);
-alert(fruta.cor);*/
-/*
+alert(fruta.cor);
+
 var frutas = [{nome:"Maçã",cor:"Vermelha"},{nome: "uva",cor:"Roxa"}];
-console.log(frutas);*/
-/*var idade = prompt("Qual sua idade?");
+console.log(frutas);
+var idade = prompt("Qual sua idade?");
 if(idade>=18){
     alert("Maior de idade")
 }else{
     alert("Menor de idade")
-}*/
-/*
+}
+
 var count = 0;
 while(count<=5){
     console.log(count);
     count ++;
-}*/
-/*
+}
+
 var count;
     for(count=0;count<=5;count++){
         alert(count);
